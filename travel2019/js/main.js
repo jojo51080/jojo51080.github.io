67118eb6e2c8f1dad5b3690f2f9d06f1ea614faf
jojo51080 <jojo51080@yahoo.com.tw>
2019-12-28 22:05:37 +0800

    1228連結

--- travel2019/js/main.js
+++ travel2019/js/main.js
@@ -9,3 +9,38 @@ $(function(){
         
     })
 })
+
+$(function(){
+    $('#img').click(function(){
+        $(this).toggleClass('min');
+        $(this).toggleClass('max');
+    });
+});
+
+
+$(function func() {
+    var btn = document.getElementById('btn');
+    var obj = document.getElementById('myarticle');
+    var total_height = obj.scrollHeight; //文章总高度
+    var show_height = 300; //定义原始显示高度
+    if (total_height > show_height) {
+        btn.style.display = 'block';
+        btn.onclick = function () {
+            obj.style.height = total_height + 'px';
+            btn.style.display = 'none';
+        };
+    }
+});
+$(function func() {
+    var btn = document.getElementById('btn2');
+    var obj = document.getElementById('myarticle2');
+    var total_height = obj.scrollHeight; //文章总高度
+    var show_height = 300; //定义原始显示高度
+    if (total_height > show_height) {
+        btn2.style.display = 'block';
+        btn2.onclick = function () {
+            obj.style.height = total_height + 'px';
+            btn.style.display = 'none';
+        };
+    }
+});
